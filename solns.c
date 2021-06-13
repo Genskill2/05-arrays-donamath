771af28af449898104768647c3ec14947255b42f
Dona Mathew <50779354+donamath@users.noreply.github.com>
2021-06-13 17:03:16 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,14 +1,13 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
 
-int max(int A[], int n){
+int max(int A[], int n)
+{
   int large=A[0],i;
-  for(i=0;i<n;i++){
-    if(A[i]>large){
+  for(i=0;i<n;i++)
+  {
+    if(A[i]>large)
       large=A[i];
-    else
-      continue;
-    }
   }
   return large;
 }
